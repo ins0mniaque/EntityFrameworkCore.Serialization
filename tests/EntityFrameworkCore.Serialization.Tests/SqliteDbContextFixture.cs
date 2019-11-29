cf7b6090f5ba14ea7a11511d07e3fef8700ba836
Jean-Philippe Leconte <ins0mniaque@gmail.com>
2019-11-29 16:21:34 -0500

    Fixed compilation warnings in tests

--- tests/EntityFrameworkCore.Serialization.Tests/SqliteDbContextFixture.cs
+++ tests/EntityFrameworkCore.Serialization.Tests/SqliteDbContextFixture.cs
@@ -7,10 +7,13 @@ namespace EntityFrameworkCore.Serialization.Tests
 {
     public abstract class SqliteDbContextFixture < TDbContext > : IDisposable where TDbContext : DbContext
     {
+        private readonly SqliteConnection disconnected;
         private readonly SqliteConnection connection;
 
         public SqliteDbContextFixture ( )
         {
+            disconnected = new SqliteConnection ( );
+
             connection = new SqliteConnection ( "DataSource=:memory:" );
             connection.Open ( );
 
@@ -18,11 +21,13 @@ namespace EntityFrameworkCore.Serialization.Tests
 
             dbContext.Database.EnsureCreated ( );
 
+            #pragma warning disable CA2214 // Do not call overridable methods in constructors
             Seed ( dbContext );
+            #pragma warning restore CA2214 // Do not call overridable methods in constructors
         }
 
-        public TDbContext CreateDbContext             ( ) => Factory ( GetDbContextOptions ( connection ) );
-        public TDbContext CreateDisconnectedDbContext ( ) => Factory ( GetDbContextOptions ( new SqliteConnection ( ) ) );
+        public TDbContext CreateDbContext             ( ) => Factory ( GetDbContextOptions ( connection   ) );
+        public TDbContext CreateDisconnectedDbContext ( ) => Factory ( GetDbContextOptions ( disconnected ) );
 
         private DbContextOptions < TDbContext > GetDbContextOptions ( SqliteConnection connection )
         {
@@ -33,6 +38,21 @@ namespace EntityFrameworkCore.Serialization.Tests
         protected abstract TDbContext Factory ( DbContextOptions < TDbContext > options );
         protected abstract void       Seed    ( TDbContext db );
 
-        public void Dispose ( ) => connection.Dispose ( );
+        public void Dispose ( )
+        {
+            Dispose ( true );
+            GC.SuppressFinalize ( this );
+        }
+
+        ~SqliteDbContextFixture ( ) => Dispose ( false );
+
+        protected virtual void Dispose ( bool disposing )
+        {
+            if ( disposing )
+            {
+                disconnected.Dispose ( );
+                connection  .Dispose ( );
+            }
+        }
     }
 }
